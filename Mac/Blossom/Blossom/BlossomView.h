57040b0c2f30c11647d43b624ac9f096ea3cb21f
Sergio Larrodera <serjpinski@gmail.com>
2018-11-19 18:29:59 +0100

    Add initial Mac code

---
+++ Mac/Blossom/Blossom/BlossomView.h
@@ -0,0 +1,13 @@
+//
+//  BlossomView.h
+//  Blossom
+//
+//  Created by Sergio Larrodera on 19/11/2018.
+//  Copyright © 2018 serjpinski. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BlossomView : ScreenSaverView
+
+@end
---
+++ Mac/Blossom/Blossom/BlossomView.m
@@ -0,0 +1,52 @@
+//
+//  BlossomView.m
+//  Blossom
+//
+//  Created by Sergio Larrodera on 19/11/2018.
+//  Copyright © 2018 serjpinski. All rights reserved.
+//
+
+#import "BlossomView.h"
+
+@implementation BlossomView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
